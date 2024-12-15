9a3d58037d1cf06def09dcbd9dbc3028b418a6f8
sarpt <mike.m.info@gmail.com>
2024-12-15 14:06:59 +0100

    Add proper help to args

--- oas_yaml_combine.py
+++ oas_yaml_combine.py
@@ -20,12 +20,12 @@ def resolve_references(input_path: str, output_path: str, refs_dir: str, inline_
 if __name__ == '__main__':
 	parser=argparse.ArgumentParser()
 
-	parser.add_argument("--input", help="Do the bar option", default="")
-	parser.add_argument("--output", help="Foo the program", default="")
-	parser.add_argument("--refsdir", help="Foo the program", default="")
-	parser.add_argument("--inlinelocal", help="Foo the program", action="store_true", default=False)
-	parser.add_argument("--inlineremote", help="Foo the program", action="store_true", default=False)
-	parser.add_argument("--keeplocal", help="Foo the program", action="store_true", default=False)
+	parser.add_argument("--input", help="path to the input yaml file to be processed. Providing --input sets the ref directory to the parent directory of provided input path. When not provided, stdin is used to read the file contents", default="")
+	parser.add_argument("--output", help="path to the output yaml file. When not provided stdout is used to return the result of documents combining", default="")
+	parser.add_argument("--refsdir", help="directory used as a root for ref relative paths resolution. By default current working directory is used, unless the input is provided", default="")
+	parser.add_argument("--inlinelocal", help="should local refs be inlined in place when resolved. When set to False, local references are left in place since they are skipped from resolving. False by default", action="store_true", default=False)
+	parser.add_argument("--inlineremote", help="should remote refs be inlined in place rather than being placed in a local equivalent. False by default. Note: remote refs are always resolved and never left in place when encountered in a document, since it's the whole point of combining documents", action="store_true", default=False)
+	parser.add_argument("--keeplocal", help="keep local refs after inlining. Makes sense only when --inlinelocal is specified as true, otherwise has no effect in order to prevent outputting incorrect yaml file with missing references", action="store_true", default=False)
 
 	args=parser.parse_args()
 
